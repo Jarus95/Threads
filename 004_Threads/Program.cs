5722d14ad0ca797dd7c120b663b9ca98b9ca202a
Jasur Sulaymonov <aliym20199@gmail.com>
2024-10-19 13:19:57 +0500

    ParameterizedThreadStart

---
+++ 004_Threads/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _004_Threads
+{
+    internal class Program
+    {
+        public static void WriteSecond(object argument)
+        {
+            Console.WriteLine(argument);
+        }
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(WriteSecond); 
+            Thread thread = new Thread(parameterizedThreadStart);
+            thread.Start("HELLO");
+            Console.ReadKey();
+        }
+    }
+}
